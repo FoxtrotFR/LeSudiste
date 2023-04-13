8ba86e7ede08d0b6f34a6a6dd1af13d87d72ecc2
maximelegac35 <130449627+maximelegac35@users.noreply.github.com>
2023-04-13 22:59:56 +0200

    menu

--- Menu.py
+++ Menu.py
@@ -15,16 +15,29 @@ def create (background, scoreboard):
     menu = Menu()
     menu.name = "LE SUDISTE"
     menu.jouer = "Appuyer sur entré pour jouer"
-
     menu.scoreboard = scoreboard
-    bg= open (background,"r")
-
-
-    menu.backgroung = bg.read().splitlines()
+    bg=open(background,"r")
+    menu.background = bg.read().splitlines()
     bg.close()
+
     return menu
 
 
 def show (menu):
-    
+    #afficher le fond
+    Game.showbackground(menu) 
+
+    #Afficher le name à l'endroit donné 
+    x=str(60)
+    y=str(5)
+    txt="\033["+y+";"+x+"H" #placer le curseur 
+    sys.stdout.write(txt)   #se placer a la position du nom 
+    sys.stdout.write(menu.name) #afficher le nom
+
+
+
+#test 
+if __name__=="__main__":
+   menu = create("background.txt",0)
+   show(menu)
     
